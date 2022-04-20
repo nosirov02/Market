4d53dfba0c241dfea4141c7a98ed8f8d4f202897
doston <dostonkhusanov@gmail.com>
2022-04-20 20:30:15 +0500

    Create User Entity

--- src/main/java/uz/isystem/market/model/User.java
+++ src/main/java/uz/isystem/market/model/User.java
@@ -1,4 +1,36 @@
 package uz.isystem.market.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Entity
+@Table(name = ("user"))
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = ("name"))
+    private String name;
+    @Column(name = ("surname"))
+    private String surname;
+    @Column(name = ("email"))
+    private String email;
+    @Column(name = ("password"))
+    private String password;
+    @Column(name = ("image_id"))
+    private Integer imageId;
+    @Column(name = ("status"))
+    private String status;
+
 }
