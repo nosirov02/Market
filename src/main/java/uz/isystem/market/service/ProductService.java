1c99d482f95b860fafd0465cbe441443a11e69e5
Abdulaziz <a.k.18072003n.b@gmail.com>
2022-04-21 01:31:16 +0500

    added create api to product

--- src/main/java/uz/isystem/market/service/ProductService.java
+++ src/main/java/uz/isystem/market/service/ProductService.java
@@ -2,10 +2,14 @@ package uz.isystem.market.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import uz.isystem.market.dto.ProductDto;
+import uz.isystem.market.exception.ServerBadRequestException;
 import uz.isystem.market.model.Product;
 import uz.isystem.market.repository.ProductRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -30,12 +34,41 @@ public class ProductService {
     }
 
 
+    public List<ProductDto> getAll() {
 
+        List<ProductDto> productDtoList = productRepository
+                .getAllByDeleted_dateIsNull()
+                .stream()
+                .map(product -> mapper.map(product, ProductDto.class))
+                .collect(Collectors.toList());
+
+        if (!productDtoList.isEmpty()){
+            for (ProductDto productDto : productDtoList){
+                setProductType(productDto);
+            }
+            return productDtoList;
+        }
+        throw new ServerBadRequestException("Products not found1");
+    }
+
+    public ProductDto getOne(Integer id) {
+        return mapper.map(getEntity(id), ProductDto.class);
+    }
 
 
 
     // |- Secondary functions -|
 
+
+
+    private Product getEntity(Integer id) {
+        Optional<Product> optionalProduct = productRepository.getByIdAndDeleted_dateIsNull(id);
+
+        if (optionalProduct.isEmpty()){
+            throw new ServerBadRequestException("Product not found!");
+        }
+        return optionalProduct.get();
+    }
 
     private void setProductType(ProductDto productDto) {
         // TODO: set productType to productDto
