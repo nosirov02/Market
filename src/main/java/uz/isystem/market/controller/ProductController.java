496cbaf31d87a39a7d3893de873a21074d2e2444
Abdulaziz <a.k.18072003n.b@gmail.com>
2022-04-21 00:43:03 +0500

    created model mapper

--- src/main/java/uz/isystem/market/controller/ProductController.java
+++ src/main/java/uz/isystem/market/controller/ProductController.java
@@ -1,11 +1,35 @@
 package uz.isystem.market.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uz.isystem.market.dto.ProductDto;
+import uz.isystem.market.service.ProductService;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    private final ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+    // |- TODO: create function -|
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody ProductDto productDto){
+        return ResponseEntity.ok(productService.create(productDto));
+    }
+
+
+    // |- TODO: getAll function -|
+
+
+    // |- TODO: getOne function -|
+
+
+    // |- TODO: update function -|
+
+
+    // |- TODO: delete function -|
 }
--- src/main/java/uz/isystem/market/service/ProductService.java
+++ src/main/java/uz/isystem/market/service/ProductService.java
@@ -1,9 +1,13 @@
 package uz.isystem.market.service;
-
 import org.springframework.stereotype.Service;
+import uz.isystem.market.dto.ProductDto;
 
 @Service
 public class ProductService {
 
 
+    public ProductDto create(ProductDto productDto) {
+
+        return productDto;
+    }
 }
