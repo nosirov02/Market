91f3a85970ea6be1883a0742e591ee2544330c1d
nabiyev0702@gmail.com <nabiyev0702@gmail.com>
2022-04-22 17:13:59 +0500

    I created all  class for user type

--- src/main/java/uz/isystem/market/model/UserType.java
+++ src/main/java/uz/isystem/market/model/UserType.java
@@ -1,10 +1,21 @@
 package uz.isystem.market.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = ("user_type"))
@@ -12,6 +23,21 @@ public class UserType {
     @Id
     @Column(name = "id", nullable = false)
     private Integer id;
+
+    @Column(name =("name"))
+    private  String name;
+
+    @Column(name =("status"))
+    private  String status;
+
+    @Column(name =("created_at"))
+    private LocalDateTime createdAt;
+
+    @Column(name =("updated_at"))
+    private LocalDateTime updatedAt;
+
+    @Column(name =("deleted_at"))
+    private LocalDateTime deletedAt;
 
 
 }
